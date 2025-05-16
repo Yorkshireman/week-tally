4590f07211da9c7959e9ae3b019b7b145fd74b9b
Yorkshireman <>
2025-05-16 11:23:54 +0100

    add text input

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
+  const [text, onChangeText] = useState('');
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>Enter things you would like to track</Text>
+        <Text style={styles.text}>
+          For best results, enter what fits in the sentence, &quot;Have you (something) today?&quot;
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder='Thing 1'
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#D0FEF5',
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  },
+  text: {
+    color: '#2D2A32',
+    fontSize: 20
+  }
+});
