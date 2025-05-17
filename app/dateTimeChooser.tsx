a970ac9ed658570e13224919f376ee05a90904f4
Yorkshireman <>
2025-05-17 22:25:07 +0100

    add half hour increments

--- app/dateTimeChooser.tsx
+++ app/dateTimeChooser.tsx
@@ -28,11 +28,17 @@ export default function DateTimeChooserScreen() {
         >
           {/* values are no. of minutes after midnight */}
           <Picker.Item label='6pm' value='1080' />
+          <Picker.Item label='6:30pm' value='1110' />
           <Picker.Item label='7pm' value='1140' />
+          <Picker.Item label='7:30pm' value='1170' />
           <Picker.Item label='8pm' value='1200' />
+          <Picker.Item label='8:30pm' value='1230' />
           <Picker.Item label='9pm' value='1260' />
+          <Picker.Item label='9:30pm' value='1270' />
           <Picker.Item label='10pm' value='1320' />
+          <Picker.Item label='10:30pm' value='1350' />
           <Picker.Item label='11pm' value='1380' />
+          <Picker.Item label='11:30pm' value='1410' />
           <Picker.Item label='midnight' value='0' />
         </Picker>
         <View>
