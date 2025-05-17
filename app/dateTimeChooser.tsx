b02ea95bcc97046ea556cd1e00ce11cced517116
Yorkshireman <>
2025-05-17 22:00:34 +0100

    add a time picker

--- app/dateTimeChooser.tsx
+++ app/dateTimeChooser.tsx
@@ -1,8 +1,11 @@
+import { Picker } from '@react-native-picker/picker';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
 import { KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, View } from 'react-native';
 import { Link, useRouter } from 'expo-router';
 
 export default function DateTimeChooserScreen() {
+  const [selectedTime, setSelectedTime] = useState();
   const router = useRouter();
 
   return (
@@ -18,6 +21,19 @@ export default function DateTimeChooserScreen() {
           The app will send you a notification at a set time of your choosing each day to ask you if
           you have done your Thing that day.
         </Text>
+        <Picker
+          selectedValue={selectedTime}
+          style={{ alignSelf: 'stretch' }}
+          onValueChange={(itemValue, itemIndex) => setSelectedTime(itemValue)}
+        >
+          <Picker.Item label='6pm' value='6pm' />
+          <Picker.Item label='7pm' value='7pm' />
+          <Picker.Item label='8pm' value='8pm' />
+          <Picker.Item label='9pm' value='9pm' />
+          <Picker.Item label='10pm' value='10pm' />
+          <Picker.Item label='11pm' value='11pm' />
+          <Picker.Item label='midnight' value='midnight' />
+        </Picker>
         <View>
           <Text style={{ ...styles.text, marginBottom: 10 }}>Finished?</Text>
           <Link href='/dateTimeChooser' style={{ ...styles.navigationButton, marginBottom: 20 }}>
