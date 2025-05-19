45755a72c863576e50e90f114ddb6dccd04ca975
Yorkshireman <>
2025-05-19 16:22:25 +0100

    add timeNow to dblogger

--- hooks/useDbLogger.ts
+++ hooks/useDbLogger.ts
@@ -4,10 +4,11 @@ export const useDbLogger = () => {
   const db = useSQLiteContext();
 
   const logDbContents = async () => {
+    const timeNow = new Date().toISOString();
     const things = await db.getAllAsync('SELECT * FROM things');
     const settings = await db.getAllAsync('SELECT * FROM settings');
     const entries = await db.getAllAsync('SELECT * FROM entries');
-    console.log('DB contents:', JSON.stringify({ entries, settings, things }, null, 2));
+    console.log('DB contents:', JSON.stringify({ entries, settings, things, timeNow }, null, 2));
   };
 
   return logDbContents;
