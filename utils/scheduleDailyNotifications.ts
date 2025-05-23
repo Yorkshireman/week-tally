69f21ad4af7a9b4cdb189eba465e56875b8d9af5
Yorkshireman <>
2025-05-23 17:31:23 +0100

    send only one daily notification asking user to update their totals

--- utils/scheduleDailyNotifications.ts
+++ utils/scheduleDailyNotifications.ts
@@ -1,18 +1,7 @@
 import * as Notifications from 'expo-notifications';
-import { NotificationDataType } from '@/types';
 import { SQLiteDatabase } from 'expo-sqlite';
 
 export const scheduleDailyNotifications = async (db: SQLiteDatabase) => {
-  const things = await db.getAllAsync<{ id: string; title: string }>(
-    'SELECT id, title FROM things;',
-    []
-  );
-
-  if (!things.length) {
-    console.error('No Things found!');
-    return;
-  }
-
   const row = await db.getFirstAsync<{ value: string }>(
     'SELECT value FROM settings WHERE key = ?',
     'askTime'
@@ -20,26 +9,22 @@ export const scheduleDailyNotifications = async (db: SQLiteDatabase) => {
 
   const askTimeMinutesAfterMidnight = Number(row?.value);
 
-  things.forEach(async thing => {
-    const notificationParams: Notifications.NotificationRequestInput = {
-      content: {
-        body: "Tap if you did, or ignore/dismiss this notification if you didn't",
-        data: { thingId: thing.id } as NotificationDataType,
-        title: `Have you done ${thing.title} today?`
-      },
-      trigger: {
-        hour: Math.floor(Number(askTimeMinutesAfterMidnight) / 60),
-        minute: askTimeMinutesAfterMidnight % 60,
-        repeats: true,
-        type: Notifications.SchedulableTriggerInputTypes.CALENDAR
-      }
-    };
+  const notificationParams: Notifications.NotificationRequestInput = {
+    content: {
+      title: 'Tap to update your notifications'
+    },
+    trigger: {
+      hour: Math.floor(Number(askTimeMinutesAfterMidnight) / 60),
+      minute: askTimeMinutesAfterMidnight % 60,
+      repeats: true,
+      type: Notifications.SchedulableTriggerInputTypes.CALENDAR
+    }
+  };
 
-    await Notifications.scheduleNotificationAsync(notificationParams);
+  await Notifications.scheduleNotificationAsync(notificationParams);
 
-    console.log(
-      'Scheduled daily notification with params: ',
-      JSON.stringify(notificationParams, null, 2)
-    );
-  });
+  console.log(
+    'Scheduled daily notification with params: ',
+    JSON.stringify(notificationParams, null, 2)
+  );
 };
