f8c52cbac7381d08150077650261c81e0655d036
Yorkshireman <>
2025-06-09 18:48:46 +0100

    add mock toggles

--- components/thingsTrackedScreen/thing.tsx
+++ components/thingsTrackedScreen/thing.tsx
@@ -1,6 +1,35 @@
-import { Text } from 'react-native';
+import { globalStyles } from '@/styles';
+import { normaliseFontSize } from '@/utils';
 import { Thing as ThingType } from '@/types';
+import { useColours } from '@/hooks';
+import { useState } from 'react';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export const Thing = ({ thing }: { thing: ThingType }) => {
-  return <Text>{thing.title}</Text>;
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const {
+    primitiveNeutral,
+    settingsScreen: { section: sectionColours }
+  } = useColours();
+
+  return (
+    <View style={{ ...globalStyles.settingsScreenSection, ...sectionColours }}>
+      <Text style={styles.text}>{thing.title}</Text>
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor='#3e3e3e'
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: normaliseFontSize(18),
+    textAlign: 'center'
+  }
+});
