0b29efa84c770c0fb4fadca6e8c7cc55673cfd27
Yorkshireman <>
2025-06-09 22:20:57 +0100

    make text a bit smalled on confirmation screen

--- app/confirmation.tsx
+++ app/confirmation.tsx
@@ -90,7 +90,7 @@ const styles = StyleSheet.create({
     textAlign: 'center'
   },
   text: {
-    fontSize: normaliseFontSize(24),
+    fontSize: normaliseFontSize(18),
     textAlign: 'center'
   }
 });
