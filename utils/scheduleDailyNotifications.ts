f07171b159731fe980e67cc4f20890757a3aea71
Yorkshireman <>
2025-06-21 15:34:41 +0100

    Modify util to allow possibility of triggering immediate notifications for devving

--- utils/scheduleDailyNotifications.ts
+++ utils/scheduleDailyNotifications.ts
@@ -1,30 +1,43 @@
 import * as Notifications from 'expo-notifications';
 import { SQLiteDatabase } from 'expo-sqlite';
 
-export const scheduleDailyNotifications = async (db: SQLiteDatabase) => {
-  const row = await db.getFirstAsync<{ value: string }>(
-    'SELECT value FROM settings WHERE key = ?',
-    'askTime'
-  );
+export const scheduleDailyNotifications = async (
+  db: SQLiteDatabase,
+  triggerImmediately?: boolean
+) => {
+  let notificationParams: Notifications.NotificationRequestInput;
+  const title = 'Tap to update your totals';
 
-  const askTimeMinutesAfterMidnight = Number(row?.value);
+  if (triggerImmediately) {
+    notificationParams = {
+      content: { title },
+      trigger: null
+    };
+  } else {
+    const row = await db.getFirstAsync<{ value: string }>(
+      'SELECT value FROM settings WHERE key = ?',
+      'askTime'
+    );
 
-  const notificationParams: Notifications.NotificationRequestInput = {
-    content: {
-      title: 'Tap to update your totals'
-    },
-    trigger: {
-      hour: Math.floor(Number(askTimeMinutesAfterMidnight) / 60),
-      minute: askTimeMinutesAfterMidnight % 60,
-      repeats: true,
-      type: Notifications.SchedulableTriggerInputTypes.CALENDAR
-    }
-  };
+    const askTimeMinutesAfterMidnight = Number(row?.value);
+
+    notificationParams = {
+      content: { title },
+      trigger: {
+        hour: Math.floor(Number(askTimeMinutesAfterMidnight) / 60),
+        minute: askTimeMinutesAfterMidnight % 60,
+        repeats: true,
+        type: Notifications.SchedulableTriggerInputTypes.CALENDAR
+      }
+    };
+  }
 
   await Notifications.scheduleNotificationAsync(notificationParams);
 
   console.log(
-    'Scheduled daily notification with params: ',
+    triggerImmediately
+      ? 'Triggered an immediate notification'
+      : 'Scheduled daily notification with params: ',
     JSON.stringify(notificationParams, null, 2)
   );
 };
